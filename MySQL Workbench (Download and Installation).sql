8345172e9c438204b94cbe0478fc766604b1f6ee
Vishuu005 <67571378+Vishuu005@users.noreply.github.com>
2022-08-23 09:41:38 +0530

    Added New Sql Installation file
    Committed

---
+++ MySQL Workbench (Download and Installation).sql
@@ -0,0 +1,57 @@
+# Step - 1.MySQL Workbench (Download and Installation)
+
+
+# MySQL Workbench is a unified visual database designing or graphical user interface tool 
+used for working with database architects, developers, and Database Administrators. 
+It is developed and maintained by Oracle. It provides SQL development, data modeling, data migration, and 
+comprehensive administration tools for server configuration, user administration, backup, and many more.
+ We can use this Server Administration for creating new physical data models, E-R diagrams, 
+ and for SQL development (run queries, etc.). It is available for all major operating systems 
+ like Mac OS, Windows, and Linux. MySQL Workbench fully supports MySQL Server version v5.6 and higher.
+
+# MySQL Workbench covers five main functionalities, which are given below:
+
+# SQL Development: This functionality provides the capability that enables you to execute SQL queries, 
+create and manage connections to the database Servers with the help of built-in SQL editor.
+
+# Data Modelling (Design): This functionality provides the capability that enables you to create models of 
+the database Schema graphically, performs reverse and forward engineering between a Schema and a live database, 
+and edit all aspects of the database using the comprehensive Table editor. The Table editor gives the facilities for editing tables, columns, indexes, views, triggers, partitioning, etc.
+
+# Server Administration: This functionality enables you to administer MySQL Server instances by administering users, 
+inspecting audit data, viewing database health, performing backup and recovery, 
+and monitoring the performance of MySQL Server.
+
+# Data Migration: This functionality allows you to migrate from Microsoft SQL Server, SQLite, Microsoft Access, PostgreSQL, Sybase ASE, SQL Anywhere, 
+and other RDBMS tables, objects, and data to MySQL. It also supports migrating from 
+the previous versions of MySQL to the latest releases.
+
+# MySQL Enterprise Supports: This functionality gives the support for Enterprise products such as
+ MySQL firewall, MySQL Enterprise Backup, and MySQL Audit.
+
+ # MySQL Workbench Environment Setup
+Here, we are going to learn how we can download and install MySQL Workbench.
+
+# Prerequisites
+The following requirements should be available in your system to work with MySQL Workbench:
+
+1.MySQL Server: You can download it from here.
+2.MySQL Workbench: You can download it from here.
+3.Microsoft .NET Framework 4.5.2
+4.Microsoft Visual C++ Redistributable for Visual Studio 2019
+5.RAM 4 GB (6 GB recommended)
+
+
+//  # Installation....................Process
+
+# Step 1: Install the MySQL Community Server. To install MySQL Server, double click the MySQL installer .exe file. After clicking the .exe file, you can see the following screen:
+ 
+  Insert into SFMC_HTML_Images (HTML_Image) 
+ Select BulkColumn FROM OPENROWSET( 
+ BULK 'mysql-workbench.png', 
+ DATA_SOURCE = 'MySQL-Tutorial', SINGLE_BLOB) AS ImageFile; 
+
+ 
+
+
+
